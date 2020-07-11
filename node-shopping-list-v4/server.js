5f5cfff8ebde94681290c451090ddc1e79083b37
RC Maples <rc@rcmaples.io>
2020-07-11 10:06:57 -0400

    Finished put recipes endpoint

--- node-shopping-list-v4/server.js
+++ node-shopping-list-v4/server.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList, Recipes} = require('./models');
+const { ShoppingList, Recipes } = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -20,10 +19,16 @@ ShoppingList.create('peppers', 4);
 
 // adding some recipes to `Recipes` so there's something
 // to retrieve.
-Recipes.create(
-  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
-Recipes.create(
-  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+Recipes.create('boiled white rice', [
+  '1 cup white rice',
+  '2 cups water',
+  'pinch of salt',
+]);
+Recipes.create('milkshake', [
+  '2 tbsp cocoa',
+  '2 cups vanilla ice cream',
+  '1 cup milk',
+]);
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
@@ -34,10 +39,10 @@ app.get('/shopping-list', (req, res) => {
 app.post('/shopping-list', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
-  for (let i=0; i<requiredFields.length; i++) {
+  for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing \`${field}\` in request body`;
       console.error(message);
       return res.status(400).send(message);
     }
@@ -54,10 +59,10 @@ app.post('/shopping-list', jsonParser, (req, res) => {
 // call `ShoppingList.update` with updated item.
 app.put('/shopping-list/:id', jsonParser, (req, res) => {
   const requiredFields = ['name', 'budget', 'id'];
-  for (let i=0; i<requiredFields.length; i++) {
+  for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing \`${field}\` in request body`;
       console.error(message);
       return res.status(400).send(message);
     }
@@ -72,7 +77,33 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
   ShoppingList.update({
     id: req.params.id,
     name: req.body.name,
-    budget: req.body.budget
+    budget: req.body.budget,
+  });
+  res.status(204).end();
+});
+
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients', 'id'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  console.log(`Updating recipe \`${req.params.id}\``);
+  Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients,
   });
   res.status(204).end();
 });
@@ -85,7 +116,6 @@ app.delete('/shopping-list/:id', (req, res) => {
   res.status(204).end();
 });
 
-
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 });
@@ -93,10 +123,10 @@ app.get('/recipes', (req, res) => {
 app.post('/recipes', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'ingredients'];
-  for (let i=0; i<requiredFields.length; i++) {
+  for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing \`${field}\` in request body`;
       console.error(message);
       return res.status(400).send(message);
     }
