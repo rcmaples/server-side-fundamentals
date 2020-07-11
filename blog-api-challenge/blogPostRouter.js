9af10a77bb3940860f1329f0125026de1faa436a
RC Maples <rc@rcmaples.io>
2020-07-11 11:09:54 -0400

    Adding v5 and blog-post challenge.

---
+++ blog-api-challenge/blogPostRouter.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const { BlogPosts } = require('./models');
+
+BlogPosts.create('Hello World', 'My first blog post', 'rcmaples');
+BlogPosts.create('Goodbye World', 'My second blog post', 'rcmaples');
+
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body.`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const post = BlogPosts.create(
+    req.body.title,
+    req.body.content,
+    req.body.author
+  );
+  res.status(201).json(post);
+});
+
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+router.put('/:id', (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body.`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id ``(${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  console.log(`Updating blog post with id \`${req.params.id}\``);
+
+  const updatedPost = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+  });
+
+  res.status(201).send(updatedPost);
+});
+
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post with id \`${req.params.id}\``);
+  res.status(204).end();
+});
+
+module.exports = router;
---
+++ blog-api-challenge/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const blogPostRouter = require('./blogPostRouter');
+
+app.use(morgan('common'));
+app.use(express.json());
+
+app.use('/blog-posts', blogPostRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`🚀 Your app is listening on port ${process.env.PORT | 8080}`);
+});
